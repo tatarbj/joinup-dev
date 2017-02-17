7b4dc024e8214c35b59d0fa21ca13c80c2156270
Sander Van Dooren <sander.vandooren@gmail.com>
2017-02-17 14:46:01 +0100

    Issue #owlimport: Create properties on the go as textfields.

--- web/modules/custom/rdf_entity/modules/owl_import/src/Form/ImportForm.php
+++ web/modules/custom/rdf_entity/modules/owl_import/src/Form/ImportForm.php
@@ -71,6 +71,10 @@ class ImportForm extends FormBase {
       if (empty($domain)) {
         continue;
       }
+      $deprecated = $property->get('owl:deprecated');
+      if ($deprecated) {
+        continue;
+      }
       $domain = $domain->getUri();
       // Property of on of the bundles?
       if (!isset($bundle_definitions[$domain])) {
@@ -92,21 +96,41 @@ class ImportForm extends FormBase {
   protected function createFields($bundles, array $property_def_by_bundle) {
     foreach ($property_def_by_bundle as $bundle_uri => $bundle_properties) {
       foreach ($bundle_properties as $property_uri => $property_def) {
-        $random = new Random();
-        $field_name = strtolower($random->name(16));
+        // $random = new Random();
+        // $field_name = strtolower($random->name(16));
         // $fieldname = strtolower($property_def['label']);.
+        $field_name = str_replace('=', '', base64_encode(sha1($property_uri)));
+        $field_name = strtolower(substr($field_name, -28));
         FieldStorageConfig::create(array(
           'field_name' => $field_name,
           'entity_type' => 'rdf_entity',
           'type' => 'text',
         ))->save();
         $label = $property_def['label'];
+        $comment = $property_def['comment'];
+        $bundle = $bundles[$bundle_uri]->id();
         FieldConfig::create([
           'field_name' => $field_name,
           'entity_type' => 'rdf_entity',
-          'bundle' => $bundles[$bundle_uri]->id(),
+          'bundle' => $bundle,
           'label' => !empty($label) ? $label->getValue() : $field_name,
+          'description' => !empty($comment) ? $comment->getValue() : '',
         ])->save();
+
+        // Assign widget settings for the 'default' form mode.
+        entity_get_form_display('rdf_entity', $bundle, 'default')
+          ->setComponent($field_name, array(
+            'type' => 'text_textfield',
+          ))
+          ->save();
+
+        // Assign display settings for the 'default' and 'teaser' view modes.
+        entity_get_display('rdf_entity', $bundle, 'default')
+          ->setComponent($field_name, array(
+            'label' => 'hidden',
+            'type' => 'text_default',
+          ))
+          ->save();
       }
     }
   }
@@ -133,6 +157,10 @@ class ImportForm extends FormBase {
           'value' => 'http://www.w3.org/2000/01/rdf-schema#label',
         ],
       ]);
+      $rdf_bundle->setThirdPartySetting('rdf_entity', 'graph', [
+        'default' => $uri,
+        'draft' => $uri,
+      ]);
       $rdf_bundle->save();
       $bundles[$uri] = $rdf_bundle;
     }
@@ -143,10 +171,16 @@ class ImportForm extends FormBase {
    * Delete all existing rdf bundles.
    */
   public function nuke() {
+    $storage = \Drupal::entityTypeManager()->getStorage('field_config');
     /** @var RdfEntityType $type */
     foreach (\Drupal::entityTypeManager()->getStorage('rdf_type')->loadMultiple() as $type) {
+      $fields = $storage->loadByProperties(['entity_type' => 'rdf_entity', 'bundle' => $type->id()]);
+      foreach ($fields as $field) {
+        $field->delete();
+      }
       $type->delete();
     }
+
   }
 
 }
